89a5049bf5e146bfe4044b2488695e8bce068cfa
Berkan Cakir <b.cakir@pm.me>
2021-06-16 16:35:22 +0200

    Commit dump.

--- experiments/covid/person.py
+++ experiments/covid/person.py
@@ -7,5 +7,78 @@ from simulation.utils import *
 
 
 class Person(Agent):
-    """ """
-    pass
+    def __init__(self, pos, v, population, index: int, mode, image: str = "experiments/covid/images/susceptible.png") -> None:
+        super(Person, self).__init__(
+            pos,
+            v,
+            color='blue',
+            index=index,
+            max_speed=config["agent"]["max_speed"],
+            min_speed=config["agent"]["min_speed"],
+            mass=config["agent"]["mass"],
+            width=config["agent"]["width"],
+            height=config["agent"]["height"],
+            dT=config["agent"]["dt"],
+        )
+
+        self.population = population
+        self.timer = 0
+        self.view_radius = 40
+        self.mode = mode
+        self.p_infection = 0.1
+        base_image, rect = image_with_rect(image, [10, 10])
+        self.rect = rect
+        self.image = base_image
+
+    def update_actions(self) -> None:
+        neighbours = self.population.find_neighbors(self, self.view_radius)
+        self.timer += 1
+
+        #print(self.image)
+
+        for obstacle in self.population.objects.obstacles:
+            collide = pygame.sprite.collide_mask(self, obstacle)
+            if bool(collide):
+                self.avoid_obstacle()
+
+        if self.timer % 50 == 0:
+            for neighbour in neighbours:
+                if neighbour.mode == 'infected' and self.mode == 'susceptible' and self.p_infection < np.random.rand():
+                    self.change_mode('infected')
+                    self.data_update()
+                    self.timer = 0
+        if self.timer > 300 and self.mode == 'infected':
+            self.change_mode('removed')
+            self.data_update()
+            self.timer = 0
+
+    def change_mode(self, mode):
+        if mode == 'infected':
+            self.mode = 'infected'
+            self.image = "experiments/covid/images/infected.png"
+        elif mode == 'removed':
+            self.mode = 'removed'
+            self.image = "experiments/covid/images/removed.png"
+        else:
+            self.mode = 'susceptible'
+            self.image = "experiments/covid/images/susceptible.png"
+
+        width = config["agent"]["width"]
+        height = config["agent"]["height"]
+        base_image, rect = image_with_rect(self.image, [width, height])
+        self.image = base_image
+
+
+    def data_update(self):
+        self.population.datapoints = []
+        for person in self.population.agents:
+            if person.mode == 'susceptible':
+                self.population.datapoints.append('S')
+            elif person.mode == 'infected':
+                self.population.datapoints.append('I')
+            elif person.mode == 'removed':
+                self.population.datapoints.append('R')
+
+    # def get_pos(self):
+    #     print(self.pos)
+    #     return self.pos
--- experiments/covid/population.py
+++ experiments/covid/population.py
@@ -21,18 +21,62 @@ class Population(Swarm):
         # To Do
         # code snipet (not complete) to avoid initializing agents on obstacles
         # given some coordinates and obstacles in the environment, this repositions the agent
-        coordinates = generate_coordinates(self.screen)
+        # coordinates = generate_coordinates(self.screen)
+        #
+        # if config["population"]["obstacles"]:  # you need to define this variable
+        #     for obj in self.objects.obstacles:
+        #         rel_coordinate = relative(
+        #             coordinates, (obj.rect[0], obj.rect[1])
+        #         )
+        #         try:
+        #             while obj.mask.get_at(rel_coordinate):
+        #                 coordinates = generate_coordinates(self.screen)
+        #                 rel_coordinate = relative(
+        #                     coordinates, (obj.rect[0], obj.rect[1])
+        #                 )
+        #         except IndexError:
+        #             pass
 
-        if config["population"]["obstacles"]:  # you need to define this variable
-            for obj in self.objects.obstacles:
-                rel_coordinate = relative(
-                    coordinates, (obj.rect[0], obj.rect[1])
-                )
-                try:
-                    while obj.mask.get_at(rel_coordinate):
-                        coordinates = generate_coordinates(self.screen)
-                        rel_coordinate = relative(
-                            coordinates, (obj.rect[0], obj.rect[1])
-                        )
-                except IndexError:
-                    pass
+        self.points_to_plot = {'S': [], 'I': [], 'R': []}
+
+        if config["base"]["scenario"] == 'A':
+            self.objects.add_object(file="experiments/covid/images/2.png",
+                                    pos=[500, 500],
+                                    scale=config["base"]["scale"],
+                                    obj_type="obstacle")
+        elif config["base"]["scenario"] == 'B':
+            self.objects.add_object(file="experiments/covid/images/2_partial.png",
+                                    pos=[500, 500],
+                                    scale=config["base"]["scale"],
+                                    obj_type="obstacle")
+        elif config["base"]["scenario"] == 'C':
+            self.objects.add_object(file="experiments/covid/images/4.png",
+                                    pos=[500, 500],
+                                    scale=config["base"]["scale"],
+                                    obj_type="obstacle")
+        elif config["base"]["scenario"] == 'D':
+            self.objects.add_object(file="experiments/covid/images/4_partial.png",
+                                    pos=[500, 500],
+                                    scale=config["base"]["scale"],
+                                    obj_type="obstacle")
+        elif config["base"]["scenario"] == 'E':
+            pass
+
+        min_x, max_x = area(config["base"]["object_location"][0], config["base"]["scale"][0])
+        min_y, max_y = area(config["base"]["object_location"][1], config["base"]["scale"][1])
+        for index, agent in enumerate(range(num_agents)):
+            coordinates = generate_coordinates(self.screen)
+
+            while (
+                    coordinates[0] >= max_x
+                    or coordinates[0] <= min_x
+                    or coordinates[1] >= max_y
+                    or coordinates[1] <= min_y
+            ):
+                coordinates = generate_coordinates(self.screen)
+
+            if index == 0:
+                mode = 'infected'
+            else:
+                mode = 'susceptible'
+            self.add_agent(Person(pos=np.array(coordinates), v=None, population=self, index=index, mode=mode))
